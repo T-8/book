42bd881cb1db0bfcb671b098530852bbe60fc0c0
T-8 <sundaradotto@gmail.com>
2020-10-02 16:17:30 +0900

    test

--- portfolio3/resources/views/books.blade.php
+++ portfolio3/resources/views/books.blade.php
@@ -29,7 +29,7 @@
 						<div class="form-group">
 							<div class="col-sm-offset-3 col-sm-6">
 								<button type="submit" class="btn btn-default">
-									<i class="fa fa-plus"></i>本を追加する
+									<i class="fa fa-plus"></i>本を追加
 								</button>
 							</div>
 						</div>
