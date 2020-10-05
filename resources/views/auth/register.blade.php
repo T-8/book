67481f6e0c69f35cd15399dc501de8fb4f28984c
T-8 <sundaradotto@gmail.com>
2020-10-05 15:20:34 +0900

    修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,22 +5,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-<<<<<<< HEAD
                 <div class="card-header">{{ __('新規登録') }}</div>
-=======
-                <div class="card-header">{{ __('Register') }}</div>
->>>>>>> 36911ca15942e3fc4ef5905348824477533c2610
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-<<<<<<< HEAD
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('名前') }}</label>
-=======
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
->>>>>>> 36911ca15942e3fc4ef5905348824477533c2610
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -34,11 +26,8 @@
                         </div>
 
                         <div class="form-group row">
-<<<<<<< HEAD
+
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Eメールアドレス') }}</label>
-=======
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
->>>>>>> 36911ca15942e3fc4ef5905348824477533c2610
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -52,11 +41,9 @@
                         </div>
 
                         <div class="form-group row">
-<<<<<<< HEAD
+
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
-=======
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
->>>>>>> 36911ca15942e3fc4ef5905348824477533c2610
+
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -70,11 +57,9 @@
                         </div>
 
                         <div class="form-group row">
-<<<<<<< HEAD
+
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('再パスワード') }}</label>
-=======
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
->>>>>>> 36911ca15942e3fc4ef5905348824477533c2610
+
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -84,11 +69,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-<<<<<<< HEAD
                                     {{ __('登録') }}
-=======
-                                    {{ __('Register') }}
->>>>>>> 36911ca15942e3fc4ef5905348824477533c2610
                                 </button>
                             </div>
                         </div>
